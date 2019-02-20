bfb37221e628111bcbbcfdf10c98e86f9d17c167
goutamahegde <47591572+goutamahegde@users.noreply.github.com>
2019-02-20 18:22:52 +0530

    Update sorting.c

--- sorting.c
+++ sorting.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 int main()
 {
       int a[4]={1,2,3,4};
       int n=4,i,j,t;
-   
       for(i=0;i<n;i++)
-      {
-            for(j=0;j<=i;j++)
-            {
-                  if(a[i]<a[j])
-                  {
-                        t=a[i];
-                        a[i]=a[j];
-                        a[j]=t;
+      { for(j=0;j<=i;j++)
+            { if(a[i]<a[j])
+                  {t=a[i];
+                    a[i]=a[j];
+                    a[j]=t;
                   }
             }
       }
