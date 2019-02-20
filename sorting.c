ffe9234b99029955cedb36e78351ad7924f12dd1
goutamahegde <47591572+goutamahegde@users.noreply.github.com>
2019-02-20 18:27:52 +0530

    Update sorting.c

--- sorting.c
+++ sorting.c
@@ -5,13 +5,15 @@ int main()
       int a[4]={1,2,3,4};
       int n=4,i,j,t;
       for(i=0;i<n;i++)
-      { for(j=0;j<=i;j++)
-            { if(a[i]<a[j])
+      { 
+            for(j=0;j<=i;j++)
+            { 
+            if(a[i]<a[j])
                   {t=a[i];
                     a[i]=a[j];
                     a[j]=t;
                   }
-            }
+             }
       }
       printf("\nAfter Sorting in Ascending Order: \n");
       for(i=0;i<n;i++)
